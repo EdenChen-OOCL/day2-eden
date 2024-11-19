0cbdfd7478e9e2c68232e14320692559c245ebb0
Eden Chen <chened11@oocl.com>
2024-11-19 15:35:42 +0800

    feat: add some field

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,3 +1,33 @@
 public class MarsRover {
 
+    private int xLocation = 0;
+    private int yLocation = 0;
+    private String direction = "N";
+
+
+
+
+    public int getxLocation() {
+        return xLocation;
+    }
+
+    public void setxLocation(int xLocation) {
+        this.xLocation = xLocation;
+    }
+
+    public int getyLocation() {
+        return yLocation;
+    }
+
+    public void setyLocation(int yLocation) {
+        this.yLocation = yLocation;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
 }
