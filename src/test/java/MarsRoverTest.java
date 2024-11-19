8c762bae69f858e624499e629145666778b5eff3
Eden Chen <chened11@oocl.com>
2024-11-19 17:18:08 +0800

    test: should turn right direction when command is R

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -50,6 +50,30 @@ public class MarsRoverTest {
     }
 
     @Test
+    public void should_turn_south_when_excuteCommand_given_current_direction_is_east_and_command_is_R() {
+        MarsRover marsRover = new MarsRover();
+        marsRover.setDirection(Direction.E.getDirection());
+        String report = marsRover.executeCommand(Command.R.getCommand());
+        assertEquals("0:0:S", report);
+    }
+
+    @Test
+    public void should_turn_west_when_excuteCommand_given_current_direction_is_south_and_command_is_R() {
+        MarsRover marsRover = new MarsRover();
+        marsRover.setDirection(Direction.S.getDirection());
+        String report = marsRover.executeCommand(Command.R.getCommand());
+        assertEquals("0:0:W", report);
+    }
+
+    @Test
+    public void should_turn_north_when_excuteCommand_given_current_direction_is_west_and_command_is_R() {
+        MarsRover marsRover = new MarsRover();
+        marsRover.setDirection(Direction.W.getDirection());
+        String report = marsRover.executeCommand(Command.R.getCommand());
+        assertEquals("0:0:N", report);
+    }
+
+    @Test
     public void should_turn_north_when_excuteCommand_given_current_direction_is_north_and_command_is_M() {
         MarsRover marsRover = new MarsRover();
         String report = marsRover.executeCommand(Command.M.getCommand());
